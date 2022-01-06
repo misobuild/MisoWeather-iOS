36d6e92decdd8b1af9945a1977d04cacb723eeba
JIINHEO <gjwldls123@gmail.com>
2022-01-06 11:53:02 +0900

    [#2] Feat: searchBar 적용

--- MisoWeather/Presentation/regionSearchViewController.swift
+++ MisoWeather/Presentation/regionSearchViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 import SnapKit
 
 class regionSearchViewController: UIViewController {
+    var num = 0
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+//        tableView.isHidden = true
+        return tableView
+    }()
     
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
@@ -27,7 +35,7 @@ class regionSearchViewController: UIViewController {
         label.font = .systemFont(ofSize: 25.0, weight: .bold)
         label.textColor = .black
         label.numberOfLines = 0
-        label.text = "파주시"
+        label.text = ""
         return label
     }()
     
@@ -42,20 +50,37 @@ class regionSearchViewController: UIViewController {
         self.navigationController?.pushViewController(nicknameSelectViewController(), animated: true)
     }
     
+    private func setNavigationItems(){
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "날씨"
+        
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "도시로 검색해주세요. 예) 파주시 "
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+        navigationItem.searchController = searchController
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setNavigationItems()
         setup()
+
     }
 }
 
 extension regionSearchViewController {
     private func setup() {
-        [titleLabel, regionLabel, confirmButton].forEach{view.addSubview($0)}
-
+        [tableView, titleLabel ,regionLabel, confirmButton].forEach{view.addSubview($0)}
+///
+        tableView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
         titleLabel.snp.makeConstraints{
             $0.leading.equalToSuperview().inset(24.0)
-            $0.top.equalToSuperview().inset(200.0)
+            $0.top.equalToSuperview().inset(300.0)
         }
         regionLabel.snp.makeConstraints{
             $0.centerX.equalToSuperview()
@@ -64,10 +89,45 @@ extension regionSearchViewController {
         }
         confirmButton.snp.makeConstraints{
             $0.centerX.equalToSuperview()
-            $0.top.equalTo(regionLabel.snp.bottom).offset(100.0)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(100.0)
             $0.leading.equalToSuperview().inset(24.0)
             $0.trailing.equalToSuperview().inset(24.0)
             $0.height.equalTo(50.0)
         }
+    }
+}
+
+extension regionSearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        num = 7
+        tableView.reloadData()
+        tableView.isHidden = false
+    }
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        num = 0
+        tableView.isHidden = true
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
+}
+
+extension regionSearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return num
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "title"
+        cell.detailTextLabel?.text = "detail"
+        
+        return cell
+    }
+}
+
+extension regionSearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = nicknameSelectViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
