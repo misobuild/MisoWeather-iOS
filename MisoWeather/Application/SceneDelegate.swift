f782e9c2046c89e2b054c1efba364de0c2bfd186
JIINHEO <gjwldls123@gmail.com>
2022-01-14 23:40:41 +0900

    [#17] Feat: Lunch Screen 적용

--- MisoWeather/Application/SceneDelegate.swift
+++ MisoWeather/Application/SceneDelegate.swift
@@ -21,6 +21,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
     
+    // LunchScreen
+    func application(_: UIApplication, didFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        sleep(2)
+        return true
+    }
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
@@ -30,12 +36,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.makeKeyAndVisible()
     }
 }
-
-// 싱글턴을 적용하던지,,, 음 객체를 어떻게 넘길 지 생각해보고
-// 카카오 ID 넘기는거 적용하고
-// 일단 카카오 ID 넘겨서 그걸로 닉네임 띄우는거 해보고
-// URL세션 적용할꺼 준비하고
-
-// 메인뷰에 콜렉션뷰 두개 적용
-// 메인뷰-1 에 콜렉션뷰 적용
-// 메인뷰-2 에 콜렉션뷰 적용
